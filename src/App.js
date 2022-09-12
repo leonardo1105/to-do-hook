1d5abd76c57b36353e6115a7924a127b2d6be2cd
Leonardo Nunes <leonardoportelanunes@gmail.com>
2022-09-12 14:34:20 -0400

    lógica pronta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  const[tarefa,setTarefa]=useState('')
+  const[listaDeTarefa,setListaDeTarefa]=useState([])
+
+  const add=() => {
+   if(tarefa.trim() !== ''){
+    setListaDeTarefa(listaDeTarefa.concat({tarefa,id:Date.now()}));
+    setTarefa('')
+   }
+  }
+
+  const remove=(id) => {
+    setListaDeTarefa(listaDeTarefa.filter(item => {
+      return item.id != id
+    }))
+  }
+
+  const handleChange=(event) => {
+    if(event.key === 'Enter'&& tarefa.trim()!== ''){
+      setListaDeTarefa(listaDeTarefa.concat({tarefa,id:Date.now()}));
+      setTarefa('')
+    }
+  }
+
+  return(
+   <>
+    <h1>To do List</h1>
+    <input value={tarefa} onKeyDown={(event) => {handleChange(event)}} onChange={(event) => {setTarefa(event.target.value)}}/>
+    <button onClick={() => {add()}}>Adicionar</button>
+    <section>{listaDeTarefa.map(item => (
+      <ul>
+        <li>{item.tarefa}</li>
+        <button onClick={() => {remove(item.id)}}>X</button>
+      </ul>
+    ))}</section>
+  </>
+  )
 }
-
-export default App;
